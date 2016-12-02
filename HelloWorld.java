933d6ba39f205ba7a3c05f697dc9706d0dfc327c
JackSias <15137615128@163.com>
2016-12-02 17:10:41 +0800

    修改HelloWorld.java文件
    第一次使用github修改文件，练习一下下

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
       public static void mian(String [] args){
-              System.out.println("lhello world"); 
+              System.out.println("lhello world11111"); 
       }
 }
